4c3ae37bd036cc037f8aac6b9090168a992dc65d
orvi2014 <das.orvi@yahoo.com>
2025-06-07 07:04:41 +0600

    resolved hydration problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  site: 'https://robatdasorvi.com',
+  integrations: [react()],
+  output: 'server', // or 'hybrid' if needed
+  adapter: vercel(),
+});
+
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'http://localhost:3000',
-  base: '/',
-  vite: {
-    ssr: {
-      noExternal: ['astro']
-    }
-  }
-});
+// export default defineConfig({
+//   site: 'http://localhost:3000',
+//   base: '/',
+//   vite: {
+//     ssr: {
+//       noExternal: ['astro']
+//     }
+//   }
+// });
